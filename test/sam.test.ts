78df55c4bc96b4fb9516521716b8f92e3a00d5bc
Kash Pitan <kash.pitan@ensek.co.uk>
2024-04-25 20:43:00 +0100

    Initial commit

---
+++ test/sam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Sam from '../lib/sam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Sam.SamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
